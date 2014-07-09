ce6f89d5e2e7c7bd523150b490276c58e8b1bdac
David Williams <david@volumesoffun.com>
2014-07-09 23:09:44 +0200

    More picking fixes.

--- Assets/Cubiquity/Scripts/Picking.cs
+++ Assets/Cubiquity/Scripts/Picking.cs
@@ -131,7 +131,7 @@ namespace Cubiquity
 			pickResult = new PickVoxelResult();
 			
 			// Can't hit it the volume if there's no data.
-			if(volume.data == null)
+			if((volume.data == null) || (volume.data.volumeHandle == null))
 			{
 				return false;
 			}
@@ -183,7 +183,7 @@ namespace Cubiquity
 			pickResult = new PickVoxelResult();
 			
 			// Can't hit it the volume if there's no data.
-			if(volume.data == null)
+			if((volume.data == null) || (volume.data.volumeHandle == null))
 			{
 				return false;
 			}
