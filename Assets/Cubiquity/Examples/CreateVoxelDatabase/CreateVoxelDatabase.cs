0876a78711e8b5b31047fcdc178288da58bd4b36
David Williams <david@volumesoffun.com>
2014-07-17 16:31:11 +0200

    Work on an example to show creation of voxel databases through code.

---
+++ Assets/Cubiquity/Examples/CreateVoxelDatabase/CreateVoxelDatabase.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Cubiquity
+{
+	public class CreateVoxelDatabase : MonoBehaviour
+	{
+		void Start ()
+		{
+			int planetRadius = 60;
+			string saveLocation = Paths.voxelDatabases + "/planet.vdb";
+			Region volumeBounds = new Region(-planetRadius, -planetRadius, -planetRadius, planetRadius, planetRadius, planetRadius);		
+			TerrainVolumeData data = VolumeData.CreateEmptyVolumeData<TerrainVolumeData>(volumeBounds, saveLocation);
+			
+			// The numbers below control the thinkness of the various layers.
+			TerrainVolumeGenerator.GeneratePlanet(data, planetRadius, planetRadius - 1, planetRadius - 10, planetRadius - 35);
+		}
+	}
+}
