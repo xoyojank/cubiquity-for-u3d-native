9b1380afc9255f0ed1b5524c17e467a6730a020e
David Williams <david@volumesoffun.com>
2014-07-22 22:01:38 +0200

    Work on solar system example.

--- Assets/Cubiquity/Examples/SolarSystem/Earth.cs
+++ Assets/Cubiquity/Examples/SolarSystem/Earth.cs
@@ -11,26 +11,9 @@ namespace CubiquityExamples
 		public float earthOrbitSpeed = 1.0f;
 		public float earthRotationSpeed = -5.0f;
 		
-		GameObject earthOrbitPoint;
-		
-		void Start()
-		{
-			earthOrbitPoint = transform.parent.gameObject;
-			
-			TerrainVolume volume = GetComponent<TerrainVolume>();
-			
-			int earthRadius = 60;
-			Region volumeBounds = new Region(-earthRadius, -earthRadius, -earthRadius, earthRadius, earthRadius, earthRadius);		
-			TerrainVolumeData result = VolumeData.CreateEmptyVolumeData<TerrainVolumeData>(volumeBounds);
-			
-			// The numbers below control the thinkness of the various layers.
-			TerrainVolumeGenerator.GeneratePlanet(result, earthRadius, earthRadius - 1, earthRadius - 10, earthRadius - 35);
-			
-			volume.data = result;
-		}
-		
 		void Update()
 		{
+			GameObject earthOrbitPoint = transform.parent.gameObject;
 			earthOrbitPoint.transform.Rotate(new Vector3(0,1,0), Time.deltaTime * earthOrbitSpeed);
 			transform.Rotate(new Vector3(0,1,0), Time.deltaTime * earthRotationSpeed);
 		}
