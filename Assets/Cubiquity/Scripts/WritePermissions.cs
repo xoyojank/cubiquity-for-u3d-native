97083a37854ea57ad954bf38aff162839d665910
David Williams <david@volumesoffun.com>
2014-07-27 21:04:25 +0200

    Added comments for WritePermissions.

--- Assets/Cubiquity/Scripts/WritePermissions.cs
+++ Assets/Cubiquity/Scripts/WritePermissions.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 namespace Cubiquity
 {
+	/// Specifies the differnt permissions with which a file can be opened.
+	/*
+	 * This is primarily used when opening voxel databases.
+	 * \sa VolumeData
+	 */
 	public enum WritePermissions
 	{
+		/// Allow only reading.
 		ReadOnly,
+		/// Allow both reading an writing.
 		ReadWrite
 	};
 }
