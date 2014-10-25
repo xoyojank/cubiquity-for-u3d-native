cd5c46b74c97c862f824b290d3075d0579c0dff5
David Williams <david@volumesoffun.com>
2014-10-25 11:00:32 +0200

    Commiting missed file.

---
+++ Assets/Cubiquity/Scripts/Impl/CuOctreeNode.cs
@@ -0,0 +1,33 @@
+﻿namespace Cubiquity
+{
+    namespace Impl
+    {
+        public struct CuOctreeNode
+        {
+            // Disable 'Field ... is never assigned to'
+            // warnings as this structure is just for interop
+#pragma warning disable 0649
+            public int posX;
+		    public int posY;
+		    public int posZ;
+
+            public uint structureLastChangedRecursive;
+
+            public uint meshLastChanged;
+            public uint meshLastChangedRecursive;
+
+		    public uint childHandle000;
+            public uint childHandle001;
+            public uint childHandle010;
+            public uint childHandle011;
+            public uint childHandle100;
+            public uint childHandle101;
+            public uint childHandle110;
+            public uint childHandle111;
+
+		    public byte hasMesh;
+		    public byte renderThisNode;
+    #pragma warning restore 0649
+        }
+    }
+}
