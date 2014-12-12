7cc6c53558d88e6aee678d2a06f7c19aebfba7c0
David Williams <david@volumesoffun.com>
2014-12-12 22:25:36 +0100

    Adding shaders for rendering colormap on heightmap.

--- Assets/Cubiquity/Scripts/Volume.cs
+++ Assets/Cubiquity/Scripts/Volume.cs
@@ -285,24 +285,29 @@ namespace Cubiquity
 				previousLayer = gameObject.layer;
 			}
 			
-			// NOTE - The following line passes transform.worldToLocalMatrix as a shader parameter. This is explicitly
-			// forbidden by the Unity docs which say:
-			//
-			//   IMPORTANT: If you're setting shader parameters you MUST use Renderer.worldToLocalMatrix instead.
-			//
-			// However, we don't have a renderer on this game object as the rendering is handled by the child OctreeNodes.
-			// The Unity doc's do not say why this is the case, but my best guess is that it is related to secret scaling 
-			// which Unity may perform before sending data to the GPU (probably to avoid precision problems). See here:
-			//
-			//   http://forum.unity3d.com/threads/153328-How-to-reproduce-_Object2World
-			//
-			// It seems to work in our case, even with non-uniform scaling applied to the volume. Perhaps we are just geting
-			// lucky, pehaps it just works on our platform, or perhaps it is actually valid for some other reason. Just be aware.
+			// Set shader parameters.
 			VolumeRenderer volumeRenderer = gameObject.GetComponent<VolumeRenderer>();
 			if(volumeRenderer != null)
 			{
 				if(volumeRenderer.material != null)
 				{
+                    Vector3i volumeSize = (data.enclosingRegion.upperCorner - data.enclosingRegion.lowerCorner);
+                    volumeSize.x++; volumeSize.y++; volumeSize.z++;
+                    volumeRenderer.material.SetVector("_VolumeSize", (Vector3)volumeSize);
+
+                    // NOTE - The following line passes transform.worldToLocalMatrix as a shader parameter. This is explicitly
+                    // forbidden by the Unity docs which say:
+                    //
+                    //   IMPORTANT: If you're setting shader parameters you MUST use Renderer.worldToLocalMatrix instead.
+                    //
+                    // However, we don't have a renderer on this game object as the rendering is handled by the child OctreeNodes.
+                    // The Unity doc's do not say why this is the case, but my best guess is that it is related to secret scaling 
+                    // which Unity may perform before sending data to the GPU (probably to avoid precision problems). See here:
+                    //
+                    //   http://forum.unity3d.com/threads/153328-How-to-reproduce-_Object2World
+                    //
+                    // It seems to work in our case, even with non-uniform scaling applied to the volume. Perhaps we are just geting
+                    // lucky, pehaps it just works on our platform, or perhaps it is actually valid for some other reason. Just be aware.
 					volumeRenderer.material.SetMatrix("_World2Volume", transform.worldToLocalMatrix);
 				}
 			}
